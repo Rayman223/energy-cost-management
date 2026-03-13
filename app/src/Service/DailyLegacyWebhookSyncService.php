9f8a2e73cb03dfb78eaa7a4c4ba18df38e407464
Rayman223 <rayman223@gmail.com>
2026-03-13 10:10:56 +0100

    fix envoi index eau a energyid

--- app/src/Service/DailyLegacyWebhookSyncService.php
+++ app/src/Service/DailyLegacyWebhookSyncService.php
@@ -255,7 +255,7 @@ final class DailyLegacyWebhookSyncService
             $points[] = [
                 'ts'    => $this->payloadFactory->unixTs($row['timestamp']),
                 // EnergyID predefined key for domestic water : 'dw'
-                'dw'   => (float) $row['value'],
+                'dw' => round((float) $row['value'] * 1000, 0),  // m³ → litres
             ];
         }
 
